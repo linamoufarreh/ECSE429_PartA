f8f5636528b913e665edb4d93f53f734af145dbb
Lina <lina.moufarreh@gmail.com>
2023-10-05 18:24:55 -0400

    sample test

--- todoAppUnit.test.js
+++ todoAppUnit.test.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 
+apiUrl= "http://localhost:4567/gui";
+
 test('should return data from API', async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    const response = await axios.get(apiUrl);
     expect(response.status).toBe(200);
     expect(response.data).toBeDefined();
 });
