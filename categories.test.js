4ba94d3e80ad06bc821d5933c21361d85f9ab42b
mehdilimitless <mammor@student.unimelb.edu.au>
2023-10-20 16:07:13 -0400

    added more tests
    Added more tests

--- categories.test.js
+++ categories.test.js
@@ -28,12 +28,70 @@ afterAll(async () => {
 
 describe('test categories', () => {
 
+    const categoryUrl = apiUrl + "/categories";
+
     //Add tests here
     test('should return data from API', async () => {
         const response = await axios.get(apiUrl);
         expect(response.status).toBe(200);
         expect(response.data).toBeDefined();
     });
+
+         // test HEAD /categories/ should return empty 
+    test("HEAD /categories returns status 200 and no body", async () => {
+        const response = await axios.head(categoryUrl);
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toBe('application/json');
+        expect(response.data).toEqual("");
+    });
+
+    test("GET /categories returns status 200 and the following JSON", async () => {
+        let response = await axios.get(categoryUrl);
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toBe('application/json');
+        
+        let expected = require('./res/categories/get_categories.json');
+
+        response = response.data.categories;
+        expected = expected.categories;
+
+        expect(response.length).toBe(expected.length);
+        expect(response).toIncludeSameMembers(expected);
+    });
+
+    // test POST /categories with valid body
+    let newCategoryID = 0;
+    test("POST /category with valid body returns status 201 and the following JSON", async () => {
+        const req = require('./res/categories/post_category_req.json');
+        const response = await axios.post(categoryUrl, req);
+
+        expect(response.status).toBe(201);
+        expect(response.headers['content-type']).toBe('application/json');
+
+        const expected = require('./res/categories/post_category_res.json');
+        expect(response.data.title).toEqual(expected.title);
+        expect(response.data.description).toEqual(expected.description);
+        
+        newCategoryID = response.data.id;
+    });
+
+    // test HEAD /categories
+    test("HEAD /categories returns status 200 and no body", async () => {
+        const response = await axios.head(categoryUrl);
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toBe('application/json');
+        expect(response.data).toEqual("");
+    }); 
+
+    // test DELETE /categories/:id
+    test("DELETE /todos/:id returns status 200 and the following JSON", async () => {
+        const response = await axios.delete(categoryUrl + "/" + newCategoryID);
+
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toBe('application/json');
+    });
+
+
 });
 
 
